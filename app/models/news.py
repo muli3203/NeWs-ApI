bf40d69e1b716559d30bb26218cd48603133bcc9
muli3203 <muli3203@gmail.com>
2020-02-03 13:38:57 +0300

    introduced  four objects

---
+++ app/models/news.py
@@ -0,0 +1,10 @@
+class News:
+    """
+News class to define News objects
+    """
+    def __init__(self,id,name,title,description):
+
+        self.id=id
+        self.name=name
+        self.title=title
+        self.description=description
