d0809f4623581fc9c0316c206111c2e9dcd15f6c
muli3203 <muli3203@gmail.com>
2020-02-03 13:34:29 +0300

    test case for init file

--- app/news_test.py
+++ app/news_test.py
@@ -2,3 +2,19 @@ import unittest
 from models import news
 News = news.News
 
+class NewsTest(unittest,TestCase):
+    '''
+    test the behaviour of News class
+    '''
+    def setUp(self):
+        '''
+        method to run before each class
+        '''
+        self.new_news = News(1234, 'Corona virus', 'flask is fucked up','moringa is also fucked fuckity up')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+
+if __name__ == "__main__":
+    unittest.main()
