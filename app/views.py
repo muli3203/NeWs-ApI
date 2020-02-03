12f755e7ef9bb4cb603e859ef60a167d43431a89
muli3203 <muli3203@gmail.com>
2020-02-03 13:34:54 +0300

    add news

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Hello World'
+    message = 'News'
     return render_template('index.html', message = message)
 
 @app.route('/news/<int:news_id>')
