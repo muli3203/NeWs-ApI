f69051713952ed748457184c9900cd188c426658
muli3203 <muli3203@gmail.com>
2020-02-07 16:02:12 +0300

    Deployment

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,10 @@ bootstrap = Bootstrap()
 
 def create_app(config_name):
 
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
-    app.config.from_pyfile('config.py')
 
     # Initializing flask extensions
     bootstrap.init_app(app)
